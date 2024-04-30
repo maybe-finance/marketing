0077593250ac1d6254e15262a1cc8a2628134ff2
Josh Pigford <josh@joshpigford.com>
2024-04-30 13:43:15 -0500

    Article controlelr

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :signups, only: [ :new, :create ]
 
+  resources :articles, only: [ :index, :show ]
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
