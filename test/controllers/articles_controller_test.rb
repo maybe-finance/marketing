a79f39d75b15f10f00a23f7b22997e42f1671528
Josh Brown <josh@joossh.com>
2024-05-01 11:15:54 +0100

    Fix failing tests

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get articles_index_url
+    get articles_url
     assert_response :success
   end
 
   test "should get show" do
-    get articles_show_url
+    get article_url(articles(:one))
     assert_response :success
   end
 end
