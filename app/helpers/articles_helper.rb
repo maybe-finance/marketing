2d4699ce7e6188559be5d9cd0e272569bcc06ac3
Josh Brown <josh@joossh.com>
2024-05-01 16:01:34 +0100

    Dynamically render author avatar based on name

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -20,4 +20,21 @@ module ArticlesHelper
   def article_gradient(article)
     ARTICLE_GRADIENTS[Zlib.crc32(article.to_param) % ARTICLE_GRADIENTS.size]
   end
+
+  def author_image_tag(name, **options)
+    image_path = "authors/#{name.parameterize}.png"
+
+    if image_exists?(image_path)
+      image_tag image_path, **options.reverse_merge(alt: name)
+    else
+      render "articles/author_placeholder", name: name, options: options
+    end
+  end
+
+  private
+
+  def image_exists?(path)
+    file_path = Rails.root.join("app", "assets", "images", path)
+    File.exist?(file_path)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,2 @@
 class Article < ApplicationRecord
-  def author_image
-    "joshpigford.png"
-  end
 end
