ce14ed06377769403a3f318730689b531db663f6
Josh Pigford <josh@joshpigford.com>
2024-05-01 13:51:13 -0500

    Update terms_controller_test.rb

--- test/controllers/terms_controller_test.rb
+++ test/controllers/terms_controller_test.rb
@@ -1,13 +1,4 @@
 require "test_helper"
 
 class TermsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get terms_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get terms_show_url
-    assert_response :success
-  end
 end
