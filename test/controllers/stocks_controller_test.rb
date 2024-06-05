e41b4b09ac176432cfc7c8747b9410f99c3ab327
Josh Pigford <josh@joshpigford.com>
2024-06-05 09:29:30 -0500

    Remove unused tests for now

--- test/controllers/stocks_controller_test.rb
+++ test/controllers/stocks_controller_test.rb
@@ -1,13 +1,4 @@
 require "test_helper"
 
 class StocksControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get stocks_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get stocks_show_url
-    assert_response :success
-  end
 end
--- test/controllers/tools_controller_test.rb
+++ test/controllers/tools_controller_test.rb
@@ -1,13 +1,4 @@
 require "test_helper"
 
 class ToolsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get tools_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get tools_show_url
-    assert_response :success
-  end
 end
