06c441790bb46375aa954b4efc23cbf967b1d75a
Josh Pigford <josh@joshpigford.com>
2024-06-06 14:55:38 -0500

    Update tool.rb

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -5,6 +5,12 @@ class Tool < ApplicationRecord
       text_class: "text-cyan-500",
       bg_class: "bg-cyan-500/5",
       solid_bg_class: "bg-cyan-500"
+    },
+    investing: {
+      name: "Investing",
+      text_class: "text-violet-500",
+      bg_class: "bg-violet-500/5",
+      solid_bg_class: "bg-violet-500"
     }
   }
 
