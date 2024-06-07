98acebe5a4724b0e8a52b5c3e0724427be74656a
Josh Pigford <josh@joshpigford.com>
2024-06-07 07:14:26 -0500

    Tools seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,45 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Tools
+tools = [
+  {
+    "name": "Financial Freedom Calculator",
+    "slug": "financial-freedom-calculator",
+    "intro": "How long will your savings last?",
+    "description": "See how long your savings will last by accounting for your monthly expenses and savings growth rate.",
+    "category_slug": "retirement",
+    "icon": "bar-chart-4"
+  },
+  {
+    "name": "ROI Calculator",
+    "slug": "roi-calculator",
+    "intro": "Calculate the return on your investments and evaluate the gain or loss.",
+    "description": "See how to calculate the return on your investments and evaluate the gain or loss.",
+    "category_slug": "investing",
+    "icon": "line-chart"
+  },
+  {
+    "name": "Loan Calculator",
+    "slug": "loan-calculator",
+    "intro": "The Loan Calculator allows you to easily plan your loans and understand your repayment schedule.",
+    "description": "See how to easily plan your loans and understand your repayment schedule.",
+    "category_slug": "debt",
+    "icon": "scale"
+  },
+  {
+    "name": "Inflation Calculator",
+    "slug": "inflation-calculator",
+    "intro": "Calculate how inflation is impacting an asset’s future price and your future buying power.",
+    "description": "See how inflation is impacting an asset’s future price and your future buying power.",
+    "category_slug": "other",
+    "icon": "infinity"
+  }
+]
+
+tools.each do |tool|
+  Tool.find_or_create_by!(slug: tool[:slug]) do |t|
+    t.name = tool[:name]
+    t.intro = tool[:intro]
+    t.description = tool[:description]
+    t.category_slug = tool[:category_slug]
+    t.icon = tool[:icon]
+  end
+end
