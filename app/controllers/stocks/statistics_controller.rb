642f1cf81d7fdbafb1c583b2d9906753d496612d
Josh Pigford <josh@joshpigford.com>
2024-06-12 09:39:33 -0500

    Stats

--- app/controllers/stocks/statistics_controller.rb
+++ app/controllers/stocks/statistics_controller.rb
@@ -1,5 +1,12 @@
 class Stocks::StatisticsController < ApplicationController
   def show
     @stock = Stock.find_by(symbol: params[:stock_ticker])
+    
+    headers = {
+      "Content-Type" => "application/json",
+      "Authorization" => "Bearer #{ENV['SYNTH_API_KEY']}"
+    }
+    @stock_statistics = Faraday.get("https://api.synthfinance.com/tickers/#{@stock.symbol}", nil, headers)
+    @stock_statistics = JSON.parse(@stock_statistics.body)["data"]
   end
 end
