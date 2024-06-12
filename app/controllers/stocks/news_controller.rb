074ba9c00d9c2f736acb0098cb1d52f98330ce4a
Josh Pigford <josh@joshpigford.com>
2024-06-11 19:38:35 -0500

    Stock news

--- app/controllers/stocks/news_controller.rb
+++ app/controllers/stocks/news_controller.rb
@@ -1,5 +1,12 @@
 class Stocks::NewsController < ApplicationController
   def show
     @stock = Stock.find_by(symbol: params[:stock_ticker])
+
+    headers = {
+      "Content-Type" => "application/json",
+      "Authorization" => "Bearer #{ENV['SYNTH_API_KEY']}"
+    }
+    @stock_news = Faraday.get("https://api.synthfinance.com/news/#{@stock.symbol}", nil, headers)
+    @stock_news = JSON.parse(@stock_news.body)["data"].first(4)
   end
 end
