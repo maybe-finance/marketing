cda4ed767c640281920bdccab23c4f6748cec0bf
Jemilu Mohammed <jemiluv8@gmail.com>
2024-06-30 23:49:23 +0000

    refactor and fix design regression

--- app/javascript/controllers/synchronized_ranges_controller.js
+++ app/javascript/controllers/synchronized_ranges_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="synchronized-ranges"
 export default class extends Controller {
   static targets = ["slider"];
-
   static locked = false;
 
   connect() {
@@ -11,50 +10,51 @@ export default class extends Controller {
   }
 
   updateSliders() {
-    const total = this.sliderTargets.reduce((sum, slider) => sum + parseInt(slider.value), 0);
+    const total = this.calculateTotal();
     if (total !== 100) {
       const diff = 100 - total;
-      const firstSlider = this.sliderTargets[0];
-      firstSlider.value = parseInt(firstSlider.value) + diff;
-      firstSlider.dispatchEvent(new Event("input"));
+      this.adjustSlider(this.sliderTargets[0], diff);
     }
   }
 
   syncSliders(event) {
-    if (this.locked) { return }
+    if (this.locked) return;
 
-    this.locked = true
-    const updatedSlider = event.target;
-    const total = this.sliderTargets.reduce((sum, slider) => sum + parseInt(slider.value), 0);
-    if (total > 100) {
-      const excess = total - 100;
-      this.adjustSliders(updatedSlider, -excess);
-    } else if (total < 100) {
-      const deficit = 100 - total;
-      this.adjustSliders(updatedSlider, deficit);
+    this.locked = true;
+    const total = this.calculateTotal();
+    const adjustment = 100 - total;
+    
+    if (adjustment !== 0) {
+      this.adjustSliders(event.target, adjustment);
     }
-    this.locked = false
+
+    this.locked = false;
   }
 
   adjustSlider(slider, share) {
     slider.value = Math.max(0, Math.min(100, parseInt(slider.value) + share));
-    slider.dispatchEvent(new Event("input"));
-
-    const changeEvent = new Event("input-sync")
-    slider.dispatchEvent(changeEvent)
+    this.dispatchInputEvent(slider);
   }
 
   adjustSliders(excludeSlider, adjustment) {
     const otherSliders = this.sliderTargets.filter(slider => slider !== excludeSlider && !slider.disabled);
 
     if (Math.abs(adjustment) === 1) {
-        this.adjustSlider(otherSliders[0], adjustment)
-        return
+      this.adjustSlider(otherSliders[0], adjustment);
+    } else {
+      const share = adjustment / otherSliders.length;
+      otherSliders.forEach(slider => this.adjustSlider(slider, share));
     }
-    const share = adjustment / otherSliders.length;
-    otherSliders.forEach(slider => this.adjustSlider(slider, share));
 
-    // This will kick in when the total exceeds and the locked/disabled slider will be ignored
-    this.updateSliders()
+    this.updateSliders();
+  }
+
+  calculateTotal() {
+    return this.sliderTargets.reduce((sum, slider) => sum + parseInt(slider.value), 0);
+  }
+
+  dispatchInputEvent(slider) {
+    slider.dispatchEvent(new Event("input"));
+    slider.dispatchEvent(new Event("input-sync"));
   }
 }
