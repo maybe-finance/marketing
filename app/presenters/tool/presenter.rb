e7f101ba8ce8125dd5c793ca140ed553bf3b35df
Jose Farias <jose@farias.mx>
2024-09-07 19:02:56 -0600

    source -> active_record

--- app/presenters/tool/presenter.rb
+++ app/presenters/tool/presenter.rb
@@ -1,10 +1,10 @@
 class Tool::Presenter
-  delegate :slug, :name, :intro, :content, :meta_image_url, to: :source
+  delegate :slug, :name, :intro, :content, :meta_image_url, to: :active_record
 
-  def initialize(source, _options)
-    @source = source
+  def initialize(active_record, _options)
+    @active_record = active_record
   end
 
   private
-    attr_reader :source
+    attr_reader :active_record
 end
