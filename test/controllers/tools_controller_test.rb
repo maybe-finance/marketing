8030ebed36c65331803def5e04195b3312792100
Jose Farias <jose@farias.mx>
2024-09-24 10:26:11 -0600

    Test tools controller

--- test/controllers/tools_controller_test.rb
+++ test/controllers/tools_controller_test.rb
@@ -1,4 +1,18 @@
 require "test_helper"
 
 class ToolsControllerTest < ActionDispatch::IntegrationTest
+  test "index" do
+    get tools_url
+    assert_response :success
+  end
+
+  test "show" do
+    get tool_url("roi-calculator")
+    assert_response :success
+  end
+
+  test "show with evil intentions" do
+    get tool_url("inaccessible-tool")
+    assert_response :not_found
+  end
 end
