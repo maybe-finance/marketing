1047527bb51ba19e5e835fee94ef0d1a4d15ab84
Jose Farias <jose@farias.mx>
2024-09-29 22:17:54 -0600

    Use more stocks in portfolio backtest tets

--- test/presenters/tool/presenter/stock_portfolio_backtest_test.rb
+++ test/presenters/tool/presenter/stock_portfolio_backtest_test.rb
@@ -3,12 +3,12 @@ require "test_helper"
 class Tool::Presenter::StockPortfolioBacktestTest < ActiveSupport::TestCase
   setup do
     @tool = Tool::Presenter::StockPortfolioBacktest.new \
-      benchmark_stock: "SCHZ",
-      investment_amount: "$10,000.00",
-      start_date: "2020-01-10",
+      benchmark_stock: "IXUS",
+      investment_amount: "$100,000.00",
+      start_date: "2020-01-07",
       end_date: "2024-09-29",
-      stocks: %w[ IXUS ],
-      stock_allocations: [ 100 ]
+      stocks: %w[ AGG SCHB SCHZ ],
+      stock_allocations: [ 33, 33, 34 ]
 
     VCR.insert_cassette "synth/stock_portfolio_backtest"
   end
@@ -27,11 +27,11 @@ class Tool::Presenter::StockPortfolioBacktestTest < ActiveSupport::TestCase
   end
 
   test "portfolio growth" do
-    assert_equal 13_104, @tool.portfolio_growth
+    assert_equal 120_098, @tool.portfolio_growth
   end
 
   test "benchmark growth" do
-    assert_equal 8_628, @tool.benchmark_growth
+    assert_equal 131_037, @tool.benchmark_growth
   end
 
   test "plot data" do
@@ -40,15 +40,15 @@ class Tool::Presenter::StockPortfolioBacktestTest < ActiveSupport::TestCase
       year: "2020",
       month: "02",
       date: "2020-02-01",
-      portfolio: 10_000,
-      benchmark: 10_000 }
+      portfolio: 100_000,
+      benchmark: 100_000 }
     last_plot_point = {
       yearMonth: "Sep 2024",
       year: "2024",
       month: "09",
       date: "2024-09-01",
-      portfolio: 13_103.695730175818,
-      benchmark: 8_628.374705562603 }
+      portfolio: 120_098.4412550996,
+      benchmark: 131_036.95730175817 }
 
     assert_equal first_plot_point, @tool.plot_data.first
     assert_equal last_plot_point, @tool.plot_data.last
