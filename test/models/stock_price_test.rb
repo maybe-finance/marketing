3ae04bd6bca0d72a8ff445503bb273588ecf098c
Jose Farias <jose@farias.mx>
2024-09-30 21:54:57 -0600

    Fix failing tests

--- test/models/stock_price_test.rb
+++ test/models/stock_price_test.rb
@@ -20,7 +20,7 @@ require "test_helper"
 class StockPriceTest < ActiveSupport::TestCase
   test "updating stock prices" do
     VCR.use_cassette "synth/known_ticker_prices" do
-      assert_difference -> { StockPrice.count }, +Stock.known_tickers.size do
+      assert_difference -> { StockPrice.count }, +StockPrice.distinct.pluck(:ticker).size do
         StockPrice.update_stock_prices
       end
     end
