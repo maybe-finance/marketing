5d383203a6a1f58675b8fbed70ea61106591f06c
Josh Pigford <josh@joshpigford.com>
2024-11-13 11:05:02 -0600

    Get similar stocks working again

--- app/controllers/stocks/similar_stocks_controller.rb
+++ app/controllers/stocks/similar_stocks_controller.rb
@@ -14,35 +14,39 @@ class Stocks::SimilarStocksController < ApplicationController
       @stock = Stock.find_by(symbol: params[:stock_ticker], country_code: "US")
     end
 
-    headers = {
-      "Content-Type" => "application/json",
-      "Authorization" => "Bearer #{ENV['SYNTH_API_KEY']}",
-      "X-Source" => "maybe_marketing",
-      "X-Source-Type" => "api"
-    }
-
-    response = Faraday.get("https://api.synthfinance.com/tickers/#{@stock.symbol}/related?mic_code=#{@stock.mic_code}", nil, headers)
-    data = JSON.parse(response.body)["data"]
-
-    @similar_stocks_data = []
-    data["related_tickers"].each do |related_stock|
-      break if @similar_stocks_data.size == 6
-
-      market_data = related_stock["market_data"]
-      next if market_data.nil? || market_data.empty?
-      next if market_data["open_today"].nil? || market_data["price_change"].nil? || market_data["percent_change"].nil?
-
-      current_price = market_data["open_today"].to_f
-      price_change = market_data["price_change"]
-      percent_change = market_data["percent_change"]
-
-      @similar_stocks_data << {
-        name: related_stock["name"],
-        symbol: related_stock["ticker"],
-        current_price: current_price,
-        price_change: price_change,
-        percent_change: percent_change
+    @similar_stocks_data = Rails.cache.fetch("similar_stocks/#{@stock.symbol}/#{@stock.mic_code}", expires_in: 6.hours) do
+      headers = {
+        "Content-Type" => "application/json",
+        "Authorization" => "Bearer #{ENV['SYNTH_API_KEY']}",
+        "X-Source" => "maybe_marketing",
+        "X-Source-Type" => "api"
       }
+
+      response = Faraday.get("https://api.synthfinance.com/tickers/#{@stock.symbol}/related?mic_code=#{@stock.mic_code}", nil, headers)
+      data = JSON.parse(response.body)["data"]
+
+      similar_stocks = []
+      data["related_tickers"].each do |related_stock|
+        break if similar_stocks.size == 6
+
+        market_data = related_stock["market_data"]
+        next if market_data.nil? || market_data.empty?
+        next if market_data["open_today"].nil? || market_data["price_change"].nil? || market_data["percent_change"].nil?
+
+        current_price = market_data["open_today"].to_f
+        price_change = market_data["price_change"]
+        percent_change = market_data["percent_change"]
+
+        similar_stocks << {
+          name: related_stock["name"],
+          symbol: related_stock["ticker"],
+          current_price: current_price,
+          price_change: price_change,
+          percent_change: percent_change
+        }
+      end
+
+      similar_stocks
     end
   end
 end
