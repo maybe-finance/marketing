dab3411e02f950ba2004cced82d1337110af1d09
Josh Pigford <josh@joshpigford.com>
2024-11-18 11:38:18 -0600

    Enable Sentry performance monitoring

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,4 +2,5 @@ Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
   config.enabled_environments = %w[production]
+  config.traces_sample_rate = 0.25
 end
