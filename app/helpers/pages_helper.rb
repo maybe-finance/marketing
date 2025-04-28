5396f2c6104017e6cf3240b0d4895d0825a16d54
Josh Pigford <josh@joshpigford.com>
2025-04-28 09:25:20 -0500

    Update FAQ content and pricing page visuals for clarity and consistency. Replace outdated questions and answers in the FAQ section, and enhance the pricing page with updated imagery and call-to-action text.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,24 +2,28 @@ module PagesHelper
   def faq_questions
     [
       {
-        question: "What's included in the Free plan?",
-        answer: "Once you sign up and create your Maybe account, you'll be able to connect up to 3 accounts. You'll also be able to do things like tracking transactions, investments and set up a budget. You will not have access to Maybe AI until you upgrade to Maybe Plus."
-      },
-      {
-        question: "What's included in Maybe Plus?",
-        answer: "Maybe Plus gives you full access to Maybe AI, lets you connect unlimited accounts across all types, and includes priority customer support. You'll also get early access to new features as they're released, along with everything already available in the Free plan."
-      },
-      {
         question: "How is my payment being processed?",
-        answer: "All payments are securely processed through Stripe. Maybe Plus is billed either monthly at $9 per month or annually at $90 per year. You'll receive an email confirmation once the payment goes through."
+        answer: "All payments are securely processed through Stripe. Maybe is billed either monthly at $9 per month or annually at $90 per year. You'll receive an email confirmation once the payment goes through."
       },
       {
         question: "What is your refund policy?",
-        answer: "If Maybe Plus doesn't work for you, we offer a full refund—no questions asked. Just reach out to us at <a href='mailto:hello@maybe.co' class='font-medium text-gray-900'>hello@maybe.co</a> and we'll take care of it."
+        answer: "If Maybe doesn't work for you, we offer a full refund—no questions asked. Just reach out to us at <a href='mailto:hello@maybe.co' class='font-medium text-gray-900'>hello@maybe.co</a> and we'll take care of it."
       },
       {
         question: "Can I cancel my subscription at any time?",
-        answer: "Yes, you can cancel whenever you like. Your Maybe Plus benefits will remain active until the end of your current billing period, and you won't be charged again after that."
+        answer: "Yes, you can cancel whenever you like. Your Maybe benefits will remain active until the end of your current billing period, and you won't be charged again after that."
+      },
+      {
+        question: "What are your data privacy & security policies?",
+        answer: "We take your data privacy and security seriously. Please refer to our <a href='https://maybe.co/privacy' class='font-medium text-gray-900'>Privacy Policy</a> for more information."
+      },
+      {
+        question: "Is  Maybe open source?",
+        answer: "Yes, Maybe is open source. You can find the source code on <a href='https://github.com/maybe-finance/maybe' class='font-medium text-gray-900'>GitHub</a>."
+      },
+      {
+        question: "Can I self-host Maybe?",
+        answer: "Yes, you can self-host Maybe. Please refer to the <a href='https://github.com/maybe-finance/maybe' class='font-medium text-gray-900'>GitHub repository</a> for more information."
       }
     ]
   end
