d4dd12ca1af34659f65effe6c1bfaa98400a0a94
Josh Pigford <josh@joshpigford.com>
2025-07-14 16:00:49 -0500

    Revert "Fix: Simplify article author management in Avo"
    This reverts commit 8f766a524b721b4b164c799af126478e54c2924f.

--- app/avo/resources/article.rb
+++ app/avo/resources/article.rb
@@ -10,19 +10,36 @@ class Avo::Resources::Article < Avo::BaseResource
 
   def fields
     field :id, as: :id
+    field :title, as: :text
+    field :slug, as: :text
+    field :content, as: :easy_mde
+    field :publish_at, as: :date_time
     field :author_name, as: :text, hide_on: [ :forms ]
+    field :author, as: :belongs_to, searchable: true, display_with_value: -> { record.name }
 
     tabs do
-      tab "General" do
-        field :title, as: :text
-        field :slug, as: :text
-        field :content, as: :easy_mde
-        field :publish_at, as: :date_time
-      end
-
-      tab "Author" do
+      tab "Author Details" do
         field :authorship, as: :has_one
       end
     end
+  end
+
+  def update_model_record(model, params, **args)
+    author_id = params.delete(:author_id)
+
+    super(model, params, **args)
+
+    if author_id.present?
+      # Remove existing authorship if present
+      model.authorship&.destroy
+
+      # Create new authorship with the selected author
+      model.create_authorship(author_id: author_id, role: "primary")
+    elsif author_id == ""
+      # If author_id is blank, remove the authorship
+      model.authorship&.destroy
+    end
+
+    model
   end
 end
